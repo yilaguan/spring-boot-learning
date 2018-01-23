8077a984efe6f436fea83e3808b5a5df6b5f3066
Dwayne Glenn <zhangliangxgd@163.com>
2018-01-23 21:40:26 +0800

    (1)修改UserController

--- src/main/java/com/nd/bigdata/controller/HelloController.java
+++ src/main/java/com/nd/bigdata/controller/HelloController.java
@@ -1,7 +1,13 @@
 package com.nd.bigdata.controller;
 
 
+import com.nd.bigdata.exceptions.MyException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,8 +20,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
-    public String index(){
+
+    private static final Log log = LogFactory.getLog(HelloController.class);
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() throws Exception{
         return "hello, world!";
+    }
+
+    @RequestMapping(value = "/hello/errors/{errorIndex}", method = RequestMethod.GET)
+    public String error(@PathVariable int errorIndex) throws Exception{
+        if(errorIndex == 1){
+            log.info("I am right!");
+        } else{
+            throw new Exception("发生错误");
+        }
+        return "正常结束";
+    }
+
+    @RequestMapping(value = "/hello/errors/jsons/{jsonId}", method = RequestMethod.GET)
+    public String jsonError(@PathVariable(name = "jsonId") int jsonId) throws MyException{
+        if(jsonId == 1){
+            log.info("It is right json");
+        } else{
+            throw new MyException("json 发生错误");
+        }
+        return "正常结束json";
+    }
+
+
+    @RequestMapping(value = "/hello/errors/global", method = RequestMethod.POST)
+    public String globalError(ModelMap map){
+        map.addAttribute("host", "http://blog.zhangliangn.com");
+        return "globalError";
     }
 }
--- src/main/java/com/nd/bigdata/controller/UserController.java
+++ src/main/java/com/nd/bigdata/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.nd.bigdata.controller;
 
+import com.nd.bigdata.dao.metadata.UserEntity;
+import com.nd.bigdata.dao.metadata.UserEntityRepository;
 import com.nd.bigdata.models.User;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -16,12 +21,18 @@ import java.util.*;
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
-    static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
+    private static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
+    private final UserEntityRepository userEntityRepository;
+
+    @Autowired
+    public UserController(UserEntityRepository userEntityRepository){
+        this.userEntityRepository = userEntityRepository;
+
+    }
 
     @RequestMapping(value="/", method = RequestMethod.GET)
-    public List<User> getUserList(){
-        List<User> r = new ArrayList<User>(users.values());
-        return r;
+    public List<UserEntity> getUserList(){
+        return userEntityRepository.findAll();
     }
 
     @RequestMapping(value="/{id}", method = RequestMethod.POST)
@@ -52,3 +63,40 @@ public class UserController {
     }
 
 }
+
+
+
+//    private static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
+//
+//    @RequestMapping(value="/", method = RequestMethod.GET)
+//    public List<User> getUserList(){
+//        List<User> r = new ArrayList<User>(users.values());
+//        return r;
+//    }
+//
+//    @RequestMapping(value="/{id}", method = RequestMethod.POST)
+//    public String postUser(@ModelAttribute User user){
+//        users.put(user.getId(), user);
+//        return "success";
+//    }
+//
+//    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+//    public User getUser(@PathVariable Long id){
+//        return users.get(id);
+//    }
+//
+//    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+//    public String putUser(@PathVariable Long id, @ModelAttribute User user){
+//        User oldUser = users.get(id);
+//        oldUser.setAge(user.getAge());
+//        oldUser.setId(user.getId());
+//        oldUser.setName(user.getName());
+//        users.put(id, oldUser);
+//        return "success";
+//    }
+//
+//    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+//    public String deleteUser(@PathVariable Long id){
+//        users.remove(id);
+//        return "success";
+//    }
